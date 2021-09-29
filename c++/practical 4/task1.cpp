6ca66bdc66c512dad5bf147a3c5489073e4d0e55
yash-khait <91232028+yash-khait@users.noreply.github.com>
2021-09-29 10:28:09 +0530

    Create task1.cpp

---
+++ c++/practical 4/task1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+namespace first
+{
+int add(int a,int b)
+{
+return a+b;
+}
+}
+namespace second
+{
+float add(float a,float b)
+{
+return a+b;
+}
+}
+using namespace first;
+int main()
+{
+cout<<add(1,2)<<endl;
+cout<<add(1.1,1.3)<<endl;
+cout<<add(1,5.1)<<endl;
+cout<<add(5.1,3)<<endl;
+return 0;
+}
