e05fd77f22ca26f774b1edcac02f0c9065d288e8
Yash-Khati <91232028+Yash-Khati@users.noreply.github.com>
2021-10-07 21:20:53 +0530

    Update task 1.cpp

--- c++/practical 5/task 1.cpp
+++ c++/practical 5/task 1.cpp
@@ -1,67 +1,61 @@
 #include<iostream>
-#include<limit.h>
-#include<float.h>
+#include<limits>
+#include<cfloat>
 using namespace std;
-void charcall()
+void data_size()
 {
-cout<<"size of char : "<<sizeof(char)<<"Range ="<<
-       char c='A';
-       cout<<c<<endl;
+
+    cout<<"------------------SIZE OF DATA TYPES -------------------\n";
+    cout<<"size of bool data type : "<<sizeof(bool)<<endl;
+    cout<<"size of short data type : "<<sizeof(short)<<endl;
+    cout<<"size of char data type : "<<sizeof(char)<<endl;
+    cout<<"size of int data type : "<<sizeof(int)<<endl;
+    cout<<"size of float data type : "<<sizeof(float)<<endl;
+    cout<<"size of long data type : "<<sizeof(long)<<endl;
+    cout<<"size of double data type  : "<<sizeof(double)<<endl;
+    cout<<"size of long double data type : "<<sizeof(long double)<<endl;
+    cout<<"size of wide char data type : "<<sizeof(wchar_t)<<endl;
 }
-void boolFunc()
+void data_varsize()
 {
-       bool b=-1;
-       if(b==true)
-       {
-              b=false;
-       }
-       cout<<b<<endl;
-}
-void shortFunc()
+    bool a=(6<2);
+    short b=871;
+    char c='A';
+    int d=8911;
+    float e=48.1234f;
+    long f=92123;
+    double g=9291.9191;
+    long double h=291291.12712;
+    wchar_t  i=L'A';
+
+    cout<<"------------------SIZE OF VARIABLES -------------------\n";
+    cout<<"size of bool data type : "<<sizeof(a)<<endl;
+    cout<<"size of short data type : "<<sizeof(b)<<endl;
+    cout<<"size of char data type : "<<sizeof(c)<<endl;
+    cout<<"size of int data type : "<<sizeof(d)<<endl;
+    cout<<"size of float data type : "<<sizeof(e)<<endl;
+    cout<<"size of long data type : "<<sizeof(f)<<endl;
+    cout<<"size of double data type  : "<<sizeof(g)<<endl;
+    cout<<"size of long double data type : "<<sizeof(h)<<endl;
+    cout<<"size of wide char data type : "<<sizeof(i)<<endl;
+}   
+void data_range()
 {
-       short s=23;
-       cout<<s<<endl;
-}
-void intFunc()
-{
-       int i=199;
-       cout<<i<<endl;
-}
-void longFunc()
-{
-       long l=111111111;
-       cout<<l<<endl;
-}
-void floatFunc()
-{
-       float f=1.111111f;
-       cout<<f<<endl;
-}
-void doubleFunc()
-{
-       double d=1.11111111111111;
-       cout<<d<<endl;
-}
-void longDoubleFunc()
-{
-       long double ld=1.11111111111111;
-       cout<<ld<<endl;
-}
-void wideFunc()
-{
-       wchar_t w=L'A';
-       cout<<w<<endl;
+    cout<<"=------------------RANGE OF DATA TYPES-------------------\n";
+    cout<<"Bool shows only 2 values : 0 or 1"<<endl;
+    cout<<"Range of short ( "<<SHRT_MIN <<" to "<<SHRT_MAX<<" )"<<endl;
+    cout<<"Range of char ( "<<CHAR_MIN <<" to "<<CHAR_MAX<<" )"<<endl;
+    cout<<"Range of int ( "<<INT_MIN <<" to "<<INT_MAX<<" )"<<endl;
+    cout<<"Range of float ( "<<FLT_MIN<<" to "<<FLT_MAX<<" )"<<endl;
+    cout<<"Range of long ( "<<LONG_MIN <<" to "<<LONG_MAX<<" )"<<endl;
+    cout<<"Range of double ( "<<CHAR_MIN <<" to "<<CHAR_MAX<<" )"<<endl;
+    cout<<"Range of long double ( "<<LDBL_MIN<<" to "<<LDBL_MAX<<" )"<<endl;
+    cout<<"Range of wide char ( "<<WCHAR_MIN <<" to "<<WCHAR_MAX<<" )"<<endl;
 }
 int main()
 {
-       charcall();
-       boolcall();
-       shortcall();
-       intcall();
-       longcall();
-       floatcall();
-       doublecall();
-       longDoublecall();
-       widecall();
-       return 0;
+ data_size();
+ data_varsize();
+ data_range();
+return 0;
 }
