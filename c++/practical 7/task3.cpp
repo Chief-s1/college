e9ec2c9a9406ba4a70994a1275a071f7861a0fe3
Yash-Khati <91232028+Yash-Khati@users.noreply.github.com>
2021-10-28 19:08:26 +0530

    Create task3.cpp

---
+++ c++/practical 7/task3.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ 
+    int a[3][3];
+    cout<<"Enter the elements\n";
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+        cin>>a[i][j];
+        a[i][j]=10-a[i][j];
+        }
+    } 
+    cout<<"The Array :\n";
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        cout<<a[i][j]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
